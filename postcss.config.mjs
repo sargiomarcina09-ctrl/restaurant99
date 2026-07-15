595e8c1559b4850621193e59c9df3f936a61da87
sargiomarcina09-ctrl <sargiomarcina09@gmail.com>
2026-07-15 15:41:47 +0530

    Add files via upload

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,5 @@
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
 
-export default config;
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
